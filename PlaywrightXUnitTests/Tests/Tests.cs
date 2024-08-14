47b31016d155239afb242eaa9856377ca816ebc2
David <d.hollander@betabit.nl>
2024-08-14 13:09:08 +0200

    changed the tests to demoqa tests

--- PlaywrightXUnitTests/Tests/Tests.cs
+++ PlaywrightXUnitTests/Tests/Tests.cs
@@ -3,33 +3,32 @@ using PlaywrightXUnit.Driver;
 
 namespace PlaywrightTests.Tests;
 
-public class HLTVTests : IClassFixture<PlaywrightDriverInitializer>
+public class Tests : IClassFixture<PlaywrightDriverInitializer>
 {
     private PlaywrightDriver _playwrightDriver;
     private BrowserSettings _browserSettings;
 
-    public HLTVTests(PlaywrightDriverInitializer playwrightDriverInitializer)
+    public Tests(PlaywrightDriverInitializer playwrightDriverInitializer)
     {
         _browserSettings = ConfigReader.ReadConfig();
         _playwrightDriver = new PlaywrightDriver(_browserSettings, playwrightDriverInitializer);
     }
 
     [Fact]
-    public async Task CheckFantasyPage()
+    public async Task OpenTextBox()
     {
         var page = await _playwrightDriver.Page;
         await page.GotoAsync(_browserSettings.Url);
-        await page.ClickAsync("text=Allow all cookies");
-        await page.ClickAsync("text=Fantasy");
+        await page.ClickAsync("text=Elements");
+        await page.ClickAsync("text=Text Box");
     }
     
     [Fact]
-    public async Task CheckTeamRanking()
+    public async Task OpenAccordian()
     {
         var page = await _playwrightDriver.Page;
         await page.GotoAsync(_browserSettings.Url);
-        await page.ClickAsync("text=Allow all cookies");
-        await page.ClickAsync("text=Complete ranking");
+        await page.ClickAsync("text=Widgets");
+        await page.ClickAsync("text=Accordian");
     }
-
 }
